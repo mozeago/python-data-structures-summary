c3c85aa8b788c242ee0d0ba38f7b6912787f9b61
Moses Asiago <mozzeago@gmail.com>
2021-12-02 19:28:58 +0300

    completed tree implementation and recursion

--- list.py
+++ list.py
@@ -1,4 +1,4 @@
-months=['January','February','March','April','May','June','July']
-#print(months[1:6])#this print from index 1(included) to index 5, index 6 is not included.
-months[1]=1
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July']
+# print(months[1:6])#this print from index 1(included) to index 5, index 6 is not included.
+months[1] = 1
 print(months[0:7])
--- trees.py
+++ trees.py
@@ -1,19 +1,58 @@
-class Node:
-    def __init__(self, letter):
-        self.childleft = None
-        self.childright = None
-        self.nodedata = letter
+class TreeNode:
+    # The TreeNode is a representation of each node element in the tree
+    def __init__(self, dataPassed):
+        # self is the current object that will be created from the class
+        # None is used for null values for variable or objects and its data type of class NoneType
+        # all variables assigned None point to the same object
+        # __init__ is used to create memory so that it can remember the variable that are created
+        # data will be waht you would like to represent, either string, letters or numbers
+        self.data = dataPassed
+        # the children will be an instance of the TreeNode class
+        self.children = []
+        # The parent of this instance of the TreeNode
+        self.parent = None
+
+    def add_child(self, child):
+        child.parent = self
+        self.children.append(child)
+
+    def print_tree(self):
+        spaces = '  ' * self.get_tree_level()
+        prefix = spaces + "|__" if self.parent else ""
+        print(prefix + self.data)
+        if self.children:
+            for child in self.children:
+                child.print_tree()
+
+    def get_tree_level(self):
+        level = 0
+        my_level = self.parent
+        while my_level:
+            level += 1
+            my_level = my_level.parent
+        return level
 
 
-root = Node('A')
-root.childleft = Node('B')
-root.childright = Node('C')
-root.childleft.childleft = Node('D')
-root.childright.childright = Node('E')
+def build_product_tree():
+    # Electronics will be stored in self.data----the varialble you passed into the class instance.
+    ceo = TreeNode("CEO")
+    country_manager = TreeNode("Country Manager")
+    unit_manager = TreeNode("Unit manager")
+    factory_manager = TreeNode("Factory manager")
+    amina = TreeNode("Quality clerk")
+    mary = TreeNode("Chief clerk")
+    john = TreeNode("Safety Clerk")
+    ceo.add_child(country_manager)
+    ceo.add_child(unit_manager)
+    ceo.add_child(factory_manager)
+    unit_manager.add_child(amina)
+    unit_manager.add_child(mary)
+    factory_manager.add_child(john)
+    return ceo
 
-   def InOrd(root):
-        if root:
-            InOrd(root.childleft)
-            print(root.nodedata)
-            InOrd(root.childright)
-    InOrd(root)
+
+# This is used as the entry to the program
+if __name__ == '__main__':
+    ceo = build_product_tree()
+    ceo.print_tree()
+pass
