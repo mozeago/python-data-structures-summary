3c8090fd4dc2c60857b06a412009aea8386a9685
Moses Asiago <mozzeago@gmail.com>
2022-01-11 08:05:51 +0300

    completed linked list insert at the end

--- linkedlist.py
+++ linkedlist.py
@@ -1,7 +1,7 @@
 class Node:
-    def __init__(self, data, nextNode=None):
+    def __init__(self, data, next_node=None):
         self.data = data
-        self.next = nextNode
+        self.next = next_node
 
 
 class LinkedList:
@@ -23,10 +23,26 @@ class LinkedList:
             iterate_linked_list = iterate_linked_list.next
         print(linked_list_string)
 
+    def insert_at_the_end(self, data):
+        if self.head is None:
+            self.insert_at_beginning(data)
+            # we return here to void having 41-->41-->14. ie double execution of statements
+            return
+        iterate_linked_list = self.head
+        while iterate_linked_list.next:
+            iterate_linked_list = iterate_linked_list.next
+        # when we have reached the LL tail
+        iterate_linked_list.next = Node(data)
+
 
 if __name__ == '__main__':
     linked_list = LinkedList()
-    linked_list.insert_at_beginning(45)
-    linked_list.insert_at_beginning(5)
-    linked_list.insert_at_beginning(4)
+    # linked_list.insert_at_beginning(45)
+    # linked_list.insert_at_beginning(5)
+    # linked_list.insert_at_beginning(4)
+    linked_list.insert_at_the_end(41)
+    linked_list.insert_at_the_end(14)
+    linked_list.insert_at_the_end(24)
+    linked_list.insert_at_the_end(34)
+
     linked_list.print_linked_list()
