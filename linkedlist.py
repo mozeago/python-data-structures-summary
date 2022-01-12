ac48e2df158886f1dd5d9c4c2b7328364fd61bc3
Moses Asiago <mozzeago@gmail.com>
2022-01-12 07:59:20 +0300

    finished with singly linked list, inserting at beginning,end,at_index, deleting at end,beginning and at index

--- linkedlist.py
+++ linkedlist.py
@@ -33,7 +33,62 @@ class LinkedList:
         # when we have reached the LL tail
         iterate_linked_list.next = Node(data)
 
+    def delete_at_index(self, index):
+        if index < 0 or index >= self.get_linked_list_length():
+            raise Exception("Invalid index")
+            return
 
+        if index == 0:
+            self.head = self.head.next
+            return
+        count = 0
+        iterate_linked_list = self.head
+        while iterate_linked_list:
+            if count == index - 1:
+                iterate_linked_list.next = iterate_linked_list.next.next
+                break
+            count += 1
+            iterate_linked_list = iterate_linked_list.next
+
+    def get_linked_list_length(self):
+        count = 0
+        iterate_linked_list = self.head
+        while iterate_linked_list:
+            iterate_linked_list = iterate_linked_list.next
+            count += 1
+
+        return count
+
+    def insert_at_index(self, index, data):
+        if index < 0 or index >= self.get_linked_list_length():
+            raise Exception("Invalid index to insert at")
+            return
+        if index == 0:
+            self.insert_at_beginning(data)
+            return
+        count = 0
+        iterate_linked_list = self.head
+        while iterate_linked_list:
+            if count == index - 1:
+                new_node = Node(data, iterate_linked_list.next)
+                iterate_linked_list.next = new_node
+                break
+            iterate_linked_list = iterate_linked_list.next
+            count += 1
+
+    def delete_at_beginning(self):
+        self.head = self.head.next
+        return
+
+    def delete_at_end(self):
+        count = 0
+        iterate_linked_list = self.head
+        while iterate_linked_list:
+            if count == self.get_linked_list_length() - 2:
+                iterate_linked_list.next = None
+                return
+            iterate_linked_list = iterate_linked_list.next
+            count += 1
 
 
 if __name__ == '__main__':
@@ -41,10 +96,17 @@ if __name__ == '__main__':
     linked_list.insert_at_beginning(45)
     linked_list.insert_at_beginning(5)
     linked_list.insert_at_beginning(4)
-# linked_list.insert_at_the_end(41)
-# linked_list.insert_at_the_end(14)
-# linked_list.insert_at_the_end(24)
-# linked_list.insert_at_the_end(34)
-
-linked_list.print_linked_list()
-print("linked list head is:---->"+str(linked_list.head.data))
+    linked_list.print_linked_list()
+    # linked_list.insert_at_the_end(41)
+    # linked_list.insert_at_the_end(14)
+    # linked_list.insert_at_the_end(24)
+    # linked_list.insert_at_the_end(34)
+    # print("Length is:----->" + str(linked_list.get_linked_list_length()))
+    # linked_list.delete_at_index(3)
+    #linked_list.insert_at_index(1, 9)
+    #linked_list.print_linked_list()
+    #linked_list.insert_at_index(3, 37)
+    # linked_list.delete_at_beginning()
+    linked_list.delete_at_end()
+    linked_list.print_linked_list()
+    print("head is:---->" + str(linked_list.head.data))
