6e8180baa5c018b7407ea4ef2b477c9855637062
Moses Asiago <mozzeago@gmail.com>
2022-01-24 05:56:40 +0300

    finished python bubble sort

--- bubblesort.py
+++ bubblesort.py
@@ -1,15 +1,18 @@
 def bubble_sort(elements):
     elements_size = len(elements)
     for k in range(elements_size - 1):
-        for j in range(elements_size - 1):
+        swapped = False
+        for j in range(elements_size - 1 - k):
             if elements[j] > elements[j + 1]:
                 tmp = elements[j]
                 elements[j] = elements[j + 1]
                 elements[j + 1] = tmp
-
+                swapped = True
+        if not swapped:
+            break
 
 
 if __name__ == "__main__":
-    elements = [3, 67, 45, 23, 9, 11, 75, 20, 1, 2, 8]
+    elements = ["moses", "hannah", "amina", "aisha", "paul", "zainab"]
     bubble_sort(elements)
     print(elements)
